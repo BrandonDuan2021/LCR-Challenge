af5e0d27a42f3181d8ecdaad3a27f8dbeff2230b
brandon <bizzarebeta@gmail.com>
2024-05-30 14:44:35 -0400

    Improve accessibility feature

--- src/components/SubmitGroup.jsx
+++ src/components/SubmitGroup.jsx
@@ -5,10 +5,10 @@ export default function SubmitGroup() {
   return (
     <div className='flex-column'>
         <div className="terms">
-            <input type="checkbox" id="terms" name="terms"/>
+            <input type="checkbox" id="terms" name="terms" aria-label="check to agree to the terms and conditions"/>
             <label htmlFor="terms"><b>I agree to the <a href="/">terms & conditions</a></b></label>
         </div>
-        <button type="submit" id='buy-button'><b>Buy Miles</b></button>
+        <button type="submit" id='buy-button' aria-label="buy miles"><b>Buy Miles</b></button>
     </div>
     
   )
